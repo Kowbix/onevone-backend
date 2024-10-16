85253cc4cc39b79956f1b0ff6cbfbd17b83b8f67
Kowbix <kacperkowbel63@gmail.com>
2024-10-16 11:42:31 +0200

    First commit

---
+++ src/main/java/com/kkowbel/oneVone/OneVoneApplication.java
@@ -0,0 +1,13 @@
+package com.kkowbel.oneVone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneVoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneVoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkowbel/oneVone/OneVoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkowbel.oneVone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneVoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
